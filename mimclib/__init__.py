824c861032e973226a4d0c5ebf773d0b167430e7
Al Haji-Ali <a.hajiali@hw.ac.uk>
2025-06-30 15:33:40 +0100

    Removed warning from setuptools

--- mimclib/__init__.py
+++ mimclib/__init__.py
@@ -1,13 +1,15 @@
-from __future__ import absolute_import
-
 from .mimc import MIMCRun, MIMCItrData
 from . import plot
 from .db import MIMCDatabase
 from . import miproj
 
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    # For Python < 3.8
+    from importlib_metadata import version, PackageNotFoundError
 
 try:
-    import pkg_resources  # part of setuptools
-    __version__ = pkg_resources.require("mimclib")[0].version
-except:
+    __version__ = version("mimclib")
+except PackageNotFoundError:
     __version__ = "Not installed"
